8b6ec7713b2264ec0943d23575c952df0aab7500
jose luis sanchez <joseluis.sanchez@globant.com>
2023-08-01 17:42:55 -0500

    implement component-library

---
+++ link-component-library-01/src/App.tsx
@@ -0,0 +1,12 @@
+import { Card } from "react-component-library";
+import "react-component-library/styles.css";
+
+function App() {
+  return (
+    <div>
+      <Card title="hello" description="This is a greetings" />
+    </div>
+  );
+}
+
+export default App;
